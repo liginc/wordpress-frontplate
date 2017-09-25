40b1371a327edd54211813b9dc72cfd65a7cca9c
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2017-09-25 15:26:40 +0900

    SP/PCでテーマが別になることを考慮をなくしてしまってたのでsrcディレクトリの階層を戻した

--- frp.config.js
+++ frp.config.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var PACKAGE = require('./package.json');
-
 // https://github.com/frontainer/frontplate-cli/wiki/6.%E8%A8%AD%E5%AE%9A
 module.exports = function (production) {
-  global.FRP_SRC  = 'src';
+  global.FRP_SRC  = 'src/sampletheme';
   global.FRP_DEST = 'wp/wp-content/themes/sampletheme';
   return {
     clean: {
