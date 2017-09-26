5a868390e260fa9cbcaf8cfed7ec86bc2396dc1d
Kobayashi Naoki <kobayashi@liginc.co.jp>
2017-09-26 20:18:38 +0900

    テーマのバージョン番号の管理方法についての修正
    - ビルドタスクによってpackage.jsonのバージョンを割り当てていたので。

--- wp/wp-content/themes/sampletheme/functions.php
+++ wp/wp-content/themes/sampletheme/functions.php
@@ -1,9 +1,13 @@
 <?php
 /**
  * Theme version
+ * style.cssで定義されているバージョン番号を定数化しておきます。
  */
-define( 'THEME_VERSION', '<%= version %>' );
+define( 'THEME_VERSION', wp_get_theme()->get( 'Version' ) );
 
+/**
+ * 読み込む順番を保証する必要があるファイルは直接インクルードするようにしてください。
+ */
 require_once( get_template_directory() . '/inc/define.php' );
 
 /*
