e4afc58141d96b4d01c144cd2ed424e8eb6a1272
Kazuhisa Togo <kazuhisa@ktogo.net>
2018-09-03 13:00:33 +0900

    Snakecasing importTemplate and importPart

--- wp/wp-content/themes/sampletheme/private/functions/01_helper_functions.php
+++ wp/wp-content/themes/sampletheme/private/functions/01_helper_functions.php
@@ -146,7 +146,7 @@ function xss( $str = null ) {
  * @param       $tpl
  * @param array $vars
  */
-function importTemplate( $tpl, $vars = array() ) {
+function import_template( $tpl, $vars = array() ) {
   $tpl  = ltrim( $tpl, '/' ) . '.php';
   $path = locate_template( array( $tpl ) );
   if ( empty( $path ) ) {
@@ -156,8 +156,18 @@ function importTemplate( $tpl, $vars = array() ) {
   include $path;
 }
 
+function import_part( $tpl, $vars = array() ) {
+  import_template( 'parts/' . ltrim( $tpl, '/' ), $vars );
+}
+
+// Old function name support for backword compatibility
+function importTemplate( $tpl, $vars = array() ) {
+  trigger_error('The function importTemplate was renamed to import_template and is now deprecated.', E_USER_DEPRECATED);
+  return call_user_func_array('import_template', func_get_args());
+}
 function importPart( $tpl, $vars = array() ) {
-  importTemplate( 'parts/' . ltrim( $tpl, '/' ), $vars );
+  trigger_error('The function importPart was renamed to import_part and is now deprecated.', E_USER_DEPRECATED);
+  return call_user_func_array('import_part', func_get_args());
 }
 
 
