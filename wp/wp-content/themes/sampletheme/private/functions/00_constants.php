3de550c5d372bbe8c0a4731d99e4091c9cbb9e74
Kazuhisa Togo <kazuhisa@ktogo.net>
2018-09-03 13:02:01 +0900

    Remove path related constants

--- wp/wp-content/themes/sampletheme/private/functions/00_constants.php
+++ wp/wp-content/themes/sampletheme/private/functions/00_constants.php
@@ -8,14 +8,6 @@
 /** TOPページの投稿取得件数 */
 //define("TOP_POST_LIMIT", "999");
 
-// home url
-define( 'HOME_URL', home_url() . '/' );
-
-// テーマディレクトリまでのパス
-define( 'ITEM_URL', get_stylesheet_directory_uri() . '/' );
-
-// ノーイメージパス
-define( 'NOIMAGE', ITEM_URL . 'assets/images/noimage.png' );
 
 $load_release_hash = function(){
   $map = get_template_directory() . '/anticache.json';
