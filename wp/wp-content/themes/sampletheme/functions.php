994cbc29cdc785134cc0e765c88be0f38039daa2
Kazuhisa Togo <kazuhisa@ktogo.net>
2018-09-03 12:42:38 +0900

    Put inc subfiles into functions

--- wp/wp-content/themes/sampletheme/functions.php
+++ wp/wp-content/themes/sampletheme/functions.php
@@ -5,11 +5,6 @@
  */
 define( 'THEME_VERSION', wp_get_theme()->get( 'Version' ) );
 
-/**
- * 読み込む順番を保証する必要があるファイルは直接インクルードするようにしてください。
- */
-require_once( get_template_directory() . '/inc/define.php' );
-
 /*
  * functionsフォルダにあるファイルをすべて読み込む
 */
