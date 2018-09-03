673594256f299da5d9f903057355a0b0602f81b5
Kazuhisa Togo <kazuhisa@ktogo.net>
2018-09-03 14:59:08 +0900

    Fix resolve_asset_url

--- wp/wp-content/themes/sampletheme/private/functions/02_path_functions.php
+++ wp/wp-content/themes/sampletheme/private/functions/02_path_functions.php
@@ -1,22 +1,20 @@
 <?php
 
 /**
- * get_template_part の変わりに利用してください。
- * $args をローカルスコープにて渡すことが可能です。
+ * アセットディレクトリ配下の指定された $subpath の完全URLを生成します
  *
- * @param       $tpl
- * @param array $vars
+ * @param string $subpath
+ * @return string
  */
 function resolve_asset_url( $subpath = '' ) {
   return esc_url( add_anticache( rtrim( get_template_directory_uri(), '/' ) . '/assets/' . ltrim( $subpath, '/' ) ) );
 }
 
 /**
- * get_template_part の変わりに利用してください。
- * $args をローカルスコープにて渡すことが可能です。
+ * 指定された $path の完全URLを生成します
  *
- * @param       $tpl
- * @param array $vars
+ * @param string $path
+ * @return string
  */
 function resolve_url( $path = '' ) {
   return esc_url( get_home_url( null, $path ) );
@@ -25,8 +23,8 @@ function resolve_url( $path = '' ) {
 /**
  * 特定の $post_type のアーカイブページURLを取得します。
  *
- * @param       $post_type
- * @param array $vars
+ * @param $post_type
+ * @return string
  */
 function resolve_archive_url( $post_type ) {
   if ( false === $url = get_post_type_archive_link( $post_type ) ) {
