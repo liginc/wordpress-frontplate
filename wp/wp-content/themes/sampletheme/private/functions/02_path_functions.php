758515bbc97a67c2af2e58a01c17cea9e13fded3
Kazuhisa Togo <kazuhisa@ktogo.net>
2018-09-03 14:50:13 +0900

    Replace uri to url in some function names

--- wp/wp-content/themes/sampletheme/private/functions/02_path_functions.php
+++ wp/wp-content/themes/sampletheme/private/functions/02_path_functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function resolve_asset_uri( $subpath = '' ) {
+function resolve_asset_url( $subpath = '' ) {
   return esc_url( add_anticache( rtrim( get_template_directory_uri(), '/' ) . '/assets/' . ltrim( $subpath, '/' ) ) );
 }
 
@@ -8,7 +8,7 @@ function resolve_url( $path = '' ) {
   return esc_url( get_home_url( null, $path ) );
 }
 
-function resolve_archive_uri( $post_type ) {
+function resolve_archive_url( $post_type ) {
   if ( false === $url = get_post_type_archive_link( $post_type ) ) {
     throw new BadMethodCallException( "Unsupported/unarchivable post_type [$post_type]" );
   }
