9e3f8cd21ce4252d8c1e25dd3c5f740fcbccc8ec
Kazuhisa Togo <kazuhisa@ktogo.net>
2018-09-21 11:31:18 +0900

    Fix missing add_anticache function (fixes #27)
    This commit partially reverts commit 3a96443b2d07284739f417f1cf7d8f4fb6839fe3.

--- wp/wp-content/themes/sampletheme/private/functions/02_path_functions.php
+++ wp/wp-content/themes/sampletheme/private/functions/02_path_functions.php
@@ -39,3 +39,33 @@ function resolve_archive_url($post_type)
 
     return $url;
 }
+
+/**
+ * 静的ファイルのキャッシュ対策としてファイルにクエリパラメータを追記して返します.
+ *
+ * @param [string] $file
+ *
+ * @return string
+ */
+function add_anticache($file)
+{
+    // anticache.jsonがある場合
+    if ('' !== (string) ANTICACHE_HASH) {
+        // svg fragment identifierの可能性を考慮
+        $parts = explode('#', $file);
+        $fragment = '';
+        // #が含まれている場合
+        if (false !== strpos($file, '#')) {
+            $fragment = "#{$parts[1]}";
+        }
+        // ?が含まれていない場合
+        if (false === strpos($parts[0], '?')) {
+            $delimeter = '?';
+        } else {
+            $delimeter = '&';
+        }
+        $file = "{$parts[0]}{$delimeter}_=".ANTICACHE_HASH.$fragment;
+    }
+
+    return $file;
+}
